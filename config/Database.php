174abea20974eb29bc51e2e8c768e753043bf408
Matt Griffin <mdg.griffin@gmail.com>
2025-03-22 19:32:28 -0400

    Change port var

--- config/Database.php
+++ config/Database.php
@@ -26,7 +26,6 @@
       }
 
       try { 
-        echo "Trying to connect";
         $this->conn = new PDO($dsn, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $this->conn;
