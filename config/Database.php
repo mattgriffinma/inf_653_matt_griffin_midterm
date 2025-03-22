58c6e7c1b5298992fdfae0c6d38b3ac156be708e
Matt Griffin <mdg.griffin@gmail.com>
2025-03-22 16:35:18 -0400

    adding db connection

--- config/Database.php
+++ config/Database.php
@@ -1,23 +1,36 @@
 <?php 
   class Database {
     // DB Params
-    private $host = 'localhost';
-    private $db_name = 'quotesdb';
-    private $username = 'root';
-    private $password = '';
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
     private $conn;
+    private $port;
+
+    public function __construct(){
+      $this->username = getenv('USERNAME');
+      $this->password = getenv('PASSWORD');
+      $this->dbname = getenv('DBNAME');
+      $this->host = getenv('HOST');
+      $this->port = getenv('PORT');
+
+    }
 
     // DB Connect
     public function connect() {
-      $this->conn = null;
+      if($this->conn){
+        return $this->conn;
+      } else {
+        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+      }
 
       try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn = new PDO($dsn, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $this->conn;
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
       }
-
-      return $this->conn;
     }
   }
