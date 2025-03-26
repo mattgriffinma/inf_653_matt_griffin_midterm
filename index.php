e70855a992795fd92bf0957350bda2d435e0a345
Matt Griffin <mdg.griffin@gmail.com>
2025-03-26 01:13:37 -0400

    added index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Midterm Example</title>
+    <style>
+    body {
+        background-color: #333;
+        color: whitesmoke;
+    }
+    </style>
+</head>
+
+<body>
+    <h1>Matt Griffin INF 653 REST API</h1>
+</body>
+</html>
